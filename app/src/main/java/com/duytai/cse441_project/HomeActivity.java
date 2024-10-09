8dae7679f462e4352df44709af4149247251981e
Le Van Binh <09.levanbinh2003@gmail.com>
2024-10-09 17:11:29 +0700

    First Add home commit!- Binh

--- app/src/main/java/com/duytai/cse441_project/HomeActivity.java
+++ app/src/main/java/com/duytai/cse441_project/HomeActivity.java
@@ -1,9 +1,6 @@
 package com.duytai.cse441_project;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,25 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-    Button btn_login;
+public class HomeActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-        btn_login = (Button) findViewById(R.id.btn_login);
-        btn_login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent_login = new Intent(MainActivity.this, HomeActivity.class);
-                startActivity(intent_login);
-            }
         });
     }
 }
