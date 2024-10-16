0c41779e480adefe1ef73a9617d259db212810cf
Le Van Binh <09.levanbinh2003@gmail.com>
2024-10-16 15:52:49 +0700

    Binh commit firebase va demo lay du lieu!

--- app/src/main/java/com/duytai/cse441_project/MainActivity.java
+++ app/src/main/java/com/duytai/cse441_project/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -20,6 +21,7 @@ import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
     Button btn_login;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
