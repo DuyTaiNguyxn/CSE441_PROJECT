b72f2779b14749ea39deeef2d1915e09bfae43ff
Duy Tai <0985203883tai@gmail.com>
2024-10-19 17:23:50 +0700

    commit lay data store tu firebase

--- app/src/main/java/com/duytai/cse441_project/fragment/BookingFragment.java
+++ app/src/main/java/com/duytai/cse441_project/fragment/BookingFragment.java
@@ -1,58 +1,69 @@
 package com.duytai.cse441_project.fragment;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.duytai.cse441_project.R;
 import com.duytai.cse441_project.adapter.StoreAdapter;
 import com.duytai.cse441_project.model.Store;
-
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 import java.util.List;
 
 public class BookingFragment extends Fragment {
 
-    public BookingFragment() {
-        // Bắt buộc phải có constructor rỗng
-    }
-
-    private RecyclerView recyclerView;
+    private RecyclerView recyclerStore;
+    private StoreAdapter storeAdapter;
     private List<Store> storeList;
+    private DatabaseReference storeReference;
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        // Inflate layout và lưu vào biến view
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_booking, container, false);
 
-        // Khởi tạo RecyclerView
-        recyclerView = view.findViewById(R.id.rcv_store);
-        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));
+        // Khởi tạo RecyclerView và Adapter
+        recyclerStore = view.findViewById(R.id.rcv_store);
+        recyclerStore.setLayoutManager(new GridLayoutManager(getContext(),2));
 
-        // Khởi tạo danh sách cơ sở
         storeList = new ArrayList<>();
-        storeList.add(new Store(1, "ChickenGang Hoàng Mai", "","","","7:00 - 22:00"));
-        storeList.add(new Store(2, "ChickenGang Cau Giay", "","","","7:00 - 22:00"));
-        storeList.add(new Store(3, "ChickenGang Tay Ho", "","","","7:00 - 22:00"));
-        storeList.add(new Store(4, "ChickenGang Long Bien", "","","","7:00 - 22:00"));
+        storeAdapter = new StoreAdapter(getContext(), storeList);
+        recyclerStore.setAdapter(storeAdapter);
 
-        // Thiết lập adapter cho RecyclerView
-        StoreAdapter adapterStore = new StoreAdapter(getContext(), storeList, store -> {
-            // Xử lý khi nhấn nút Đặt bàn
+        // Tham chiếu tới Firebase Database
+        storeReference = FirebaseDatabase.getInstance().getReference("Store");
+
+        // Lấy dữ liệu từ Firebase
+        storeReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                storeList.clear();
+                for (DataSnapshot storeSnapshot : snapshot.getChildren()) {
+                    Store store = storeSnapshot.getValue(Store.class);
+                    if (store != null) {
+                        storeList.add(store);
+                    }
+                }
+                storeAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.e("StoreFragment", "Error fetching store data", error.toException());
+            }
         });
-        recyclerView.setAdapter(adapterStore);
 
-        return view; // Trả về view sau khi đã thiết lập
+        return view;
     }
 }
-
