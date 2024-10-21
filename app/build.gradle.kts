adebb0c9d623bd0d6463af68e5ac5d1a228c67cc
Duy Tai <0985203883tai@gmail.com>
2024-10-21 23:04:30 +0700

    commit chuc nang dat ban (hoan thien)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,5 +47,5 @@ dependencies {
     implementation ("com.github.bumptech.glide:glide:4.12.0")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
     implementation ("com.squareup.picasso:picasso:2.8")
-
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.2.0-alpha01")
 }
